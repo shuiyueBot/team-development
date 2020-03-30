8f187fcb898148cd02d08f24bf57055378fa049f
shuiyueBot <62690930+shuiyueBot@users.noreply.github.com>
2020-03-30 20:06:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::prefix("admin")->group(function(){
     Route::post('store',"AdminController@store");
     Route::get('create',"AdminController@create");
